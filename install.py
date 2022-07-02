4ecea725dc8ea367360e0c661a19f36d52bcd7d8
Makesi Morrison <makmorrison@gmail.com>
2022-07-02 14:06:54 -0500

    Feat: added all the necesarry functions without the logic

--- install.py
+++ install.py
@@ -9,4 +9,35 @@ def run():
     install_programs()
 
 def install_programs():
+  parser = argparse.ArgumentParser(
+      description='Installs PHP, Composer, MySQL and NodeJS on WSL (Ubuntu)'
+    )
+
+  parser.add_argument('--php', help='Installs PHP')
+  parser.add_argument('--mysql', help='Installs MySql')
+  parser.add_argument('--node', help='Installs NodeJs')
+  parser.add_argument('--all', help='Installs all programs')
+
+  args = parser.parse_args()
+
+  if args.all:
+    install_all_programs()
+    return
+  elif args.mysql:
+    install_mysql()
+  elif args.php:
+    install_php()
+  elif args.node:
+    install_node()
+
+def install_all_programs():
+  pass
+
+def install_php():
+  pass
+
+def install_mysql():
+  pass
+
+def install_node():
   pass
